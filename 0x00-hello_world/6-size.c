c50bbb56de6c71c6222ff7f66c5906c70168089f
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-09-08 15:16:46 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int intType;
+long int longIntType;
+float floatType;
+long long longLongType
+char charType;
+printf("Size of a char: %zu byte(s)\n", sizeof(chatType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long: %zu byte(s)\n", sizeof(longLongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
