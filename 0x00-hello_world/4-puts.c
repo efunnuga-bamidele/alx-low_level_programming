9fe00d84d5b6b2d4a1db4873f7c35d9a3d9ba2ea
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-09-08 14:30:11 -0700

    task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ *
+ * A C program that uses PUT function in place of printf function
+ *
+**/
+int main(void)
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(str1);
+	return (0);
+}
+
