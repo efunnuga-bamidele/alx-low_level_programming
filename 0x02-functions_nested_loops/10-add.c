57ca9231d0ee07ee0a7139f5bfaec8eb3a1b53a4
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-09-14 16:53:29 +0100

    Multiple task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Prints the sum of two integer arguments
+ * @num1: First integer is entered
+ * @num2: Second integer is entered
+ *
+ * Return: The sum of the integers is passed.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
