bb16ae9431e892e4452eee81edd233d15c0172e5
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-09-15 18:52:27 +0100

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,14 +4,12 @@
 * main - texts function that prints if integer is positive or negative_sign
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
-	
+
 	i = 98;
 	positive_or_negative(i);
 	
 	return (0);
-}
 
