50aed5446fb4dd839a7201e47089646032f60d31
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-09-15 18:19:12 +0100

    Task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - texts function that prints if integer is positive or negative_sign
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	
+	i = 98;
+	positive_or_negative(i);
+	
+	return (0);
+}
+
